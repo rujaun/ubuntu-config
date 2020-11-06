abfc1c8ec09b9a9019b631a077be2a87bd834267
Rujaun Fourie <rujaun@gmail.com>
2020-11-06 20:44:26 +0200

    Update config

--- config.sh
+++ config.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 mkdir ~/.config/i3/
-mkdir ~/.config/polybar/
+mkdir ~/.config/i3status/
+#mkdir ~/.config/polybar/
 
 cp -r config/i3/* ~/.config/i3/
-cp -r config/polybar/* ~/.config/polybar/
+cp -r config/i3status/* ~/.config/i3status/
+#cp -r config/polybar/* ~/.config/polybar/
