f5d2c410729eff88b975ef142cd773d22dc4e551
Rujaun Fourie <rujaun@gmail.com>
2020-11-06 12:15:36 +0200

    Added pywal

--- fonts.sh
+++ fonts.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-echo -e "\nInstall Windows Fonts? (Y/N): "
-
-read FONTS
-
-if [ "$FONTS" = "Y" ]; then
-
-	echo -e "\nChecking if WinFonts exists..."
-
-	FILE=./WinFonts
-	if [[ -d "$FILE" ]]; then
-		echo -e "\n$FILE exists."
-	else
-		echo -e "\n$FILE does not exist."
-		exit
-	fi
-
-	echo -e "\nInstalling fonts"
-	sudo mkdir /usr/share/fonts/WinFonts
-	sudo cp ./WinFonts/* /usr/share/fonts/WinFonts/
-	sudo chmod 644 /usr/share/fonts/WinFonts/*
-	fc-cache --force
-	sudo cp ~/popinstall/config/.local.conf /etc/fonts/.local.conf
-fi
+sudo mkdir /usr/share/fonts/WinFonts
+sudo cp ./WinFonts/* /usr/share/fonts/WinFonts/
+sudo chmod 644 /usr/share/fonts/WinFonts/*
+fc-cache -f -v
+sudo cp ~/popinstall/config/.local.conf /etc/fonts/.local.conf
