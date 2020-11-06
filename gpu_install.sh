29ebd15fbe2f31fd8ac78a7a7ad4dee38cfa12ca
Rujaun Fourie <rujaun@gmail.com>
2020-11-06 22:20:32 +0200

    Fixed typos

--- gpu_install.sh
+++ gpu_install.sh
@@ -4,15 +4,12 @@ sudo dpkg --add-architecture i386
 
 sudo apt update && sudo apt dist-upgrade
 
-sudo apt install xterm libgl1-mesa-dri libgl1-mesa-glx libgl1-mesa-glx libglu1-mesa x11-apps x11-session-utils x11-utils x11-xkb-utils x11-xserver-utils xauth xfonts-base xfonts-utils xinit xinput xkb-data xorg-docs-core xserver-xorg xfonts-scalable x11-xfs-utils xfonts-100dpi xfonts-75dpi xorg-docs --no-install-recommends --no-install-suggests
-sudo apt install arandr rxvt-unicode
-
 echo -e "\nInstall GPU drivers (nvidia | intelamd): "
 
 read GPU
 
 if [ "$GPU" = "intelamd" ]; then
-	echo -e "\nInstalling AMD GPU drivers"
+	echo -e "\nInstalling Intel/AMD GPU drivers"
 	sudo add-apt-repository ppa:kisak/kisak-mesa
 	sudo apt update && sudo apt upgrade
 	sudo apt install libgl1-mesa-dri:i386
