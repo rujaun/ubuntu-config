b082f0893cb269167c3a5e7ff64537a20a83d3e6
Rujaun Fourie <rujaun@gmail.com>
2020-11-06 15:14:43 +0200

    tweak polybar

--- polybar.sh
+++ polybar.sh
@@ -3,9 +3,10 @@
 git clone --recursive https://github.com/polybar/polybar
 cd polybar
 mkdir build
-cd build
-cmake ..
-make -j$(nproc)
-sudo make install
-cd ..
-sudo rm -r polybar
+./build.sh
+#cd build
+#cmake ..
+#make -j$(nproc)
+#sudo make install
+#cd ..
+#sudo rm -r polybar
