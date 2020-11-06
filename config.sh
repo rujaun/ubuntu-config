4468b883160b30d595c1f8cddf89e73905ad3d74
Rujaun Fourie <rujaun@gmail.com>
2020-11-06 21:28:48 +0200

    fixed config path

--- config.sh
+++ config.sh
@@ -6,8 +6,8 @@ mkdir -p ~/.config/wal/templates/
 mkdir ~/.config/rofi/
 #mkdir ~/.config/polybar/
 
-cp -r rofi/* ~/.config/rofi/
+cp -r config/rofi/* ~/.config/rofi/
 cp -r config/i3/* ~/.config/i3/
 cp -r config/i3status/* ~/.config/i3status/
-cp -r wal/* ~/.config/wal/
+cp -r config/wal/* ~/.config/wal/
 #cp -r config/polybar/* ~/.config/polybar/
