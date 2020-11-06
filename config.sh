33a2969488db3e39579fe11b33217257c25db16b
Rujaun Fourie <rujaun@gmail.com>
2020-11-06 21:46:50 +0200

    Added picom config

--- config.sh
+++ config.sh
@@ -4,10 +4,12 @@ mkdir ~/.config/i3/
 mkdir ~/.config/i3status/
 mkdir -p ~/.config/wal/templates/
 mkdir ~/.config/rofi/
+mkdir -p ~/.config/picom
 #mkdir ~/.config/polybar/
 
 cp -r config/rofi/* ~/.config/rofi/
 cp -r config/i3/* ~/.config/i3/
 cp -r config/i3status/* ~/.config/i3status/
 cp -r config/wal/* ~/.config/wal/
+cp -r config/picom/* ~/.config/picom/
 #cp -r config/polybar/* ~/.config/polybar/
