862679e56b894854b09678e41b7ca41ce7a6abef
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 15:46:14 +0200

    Install font-awesome

--- fonts.sh
+++ fonts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt install -y fonts-font-awesome
+
 sudo mkdir /usr/share/fonts/WinFonts
 sudo cp ./WinFonts/* /usr/share/fonts/WinFonts/
 sudo chmod 644 /usr/share/fonts/WinFonts/*
