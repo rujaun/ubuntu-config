b8d8922406cf3283d22337908cc78c958a2a4575
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 18:24:39 +0200

    Changed lock screen background and added arc theme

--- apps.sh
+++ apps.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y vim htop neofetch arandr pulseaudio pulseaudio-utils \
 					playerctl lm-sensors xbacklight lxappearance gnome-themes-standard \
-					breeze-cursor-theme
+					breeze-cursor-theme arc-theme
 
 sudo apt install -y nautilus --no-install-recommends --no-install-suggests
 sudo apt install -y firefox --no-install-recommends --no-install-suggests
