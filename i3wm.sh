590effe3ae75c77984d94078bf7b66d3c4f60d6b
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 12:26:23 +0200

    Added i3lock

--- i3wm.sh
+++ i3wm.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sudo apt install -y build-essential libpam0g-dev libxcb-xkb-dev
-sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev libxcb-shape0-dev automake
+
+sudo apt install -y build-essential libpam0g-dev libxcb-xkb-dev libxcb1-dev \
+					libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev \
+					libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev \
+					libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+					libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 \
+					libxcb-xrm-dev libxcb-shape0-dev automake
 
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
@@ -15,6 +20,6 @@ sudo make install
 cd ..
 sudo rm -r i3
 
-sudo apt install i3status
+sudo apt install i3status i3lock
 sudo apt install rofi
 sudo apt install lm-sensors
