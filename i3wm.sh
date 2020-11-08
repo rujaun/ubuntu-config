827dcba9b25782660bb497fd8f52cac4e067c386
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 11:57:42 +0200

    Use i3-gaps

--- i3wm.sh
+++ i3wm.sh
@@ -3,8 +3,9 @@
 sudo apt install -y build-essential libpam0g-dev libxcb-xkb-dev
 sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev libxcb-shape0-dev automake
 
-git clone https://github.com/resloved/i3.git
-cd i3
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+
 autoreconf --force --install
 rm -rf build/
 mkdir -p build && cd build/
