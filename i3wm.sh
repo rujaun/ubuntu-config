65a4f844de6a7a69c0692ca232738f99166c6197
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 18:47:45 +0200

    Remove i3lock for i3lock-color fork

--- i3wm.sh
+++ i3wm.sh
@@ -5,7 +5,10 @@ sudo apt install -y build-essential libpam0g-dev libxcb-xkb-dev libxcb1-dev \
 					libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev \
 					libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
 					libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 \
-					libxcb-xrm-dev libxcb-shape0-dev automake
+					libxcb-xrm-dev libxcb-shape0-dev automake scrot make pkg-config \
+					libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libx11-xcb-dev \
+					libxcb-image0-dev libxcb-util-dev libjpeg-dev
+
 
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
@@ -19,5 +22,13 @@ sudo make install
 cd ..
 sudo rm -r i3
 
-sudo apt install i3status i3lock
+git clone https://github.com/Raymo111/i3lock-color
+cd i3lock-color
+git tag -f "git-$(git rev-parse --short HEAD)"
+chmod +x build.sh
+./build.sh
+chmod +x install-i3lock-color.sh
+./install-i3lock-color.sh
+
+sudo apt install i3status
 sudo apt install rofi
