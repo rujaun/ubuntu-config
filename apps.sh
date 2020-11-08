165b488ae68ca7e92313cc5dd425583c79b2be14
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 16:58:04 +0200

    Removed kitty because of high memory usage

--- apps.sh
+++ apps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install -y vim htop neofetch arandr pulseaudio pulseaudio-utils \
-					playerctl lm-sensors xbacklight kitty
+					playerctl lm-sensors xbacklight
 
 sudo apt install -y nautilus --no-install-recommends --no-install-suggests
 sudo apt install -y firefox --no-install-recommends --no-install-suggests
