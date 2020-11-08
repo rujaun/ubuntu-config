d376d1793f8bef4ee9b0f712c9a45994d4710ff1
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 12:48:53 +0200

    Prevent snapd from being installed

--- snapd.sh
+++ snapd.sh
@@ -2,3 +2,7 @@
 
 sudo apt purge snapd
 sudo apt autoremove
+echo 'Package: snapd' > /etc/apt/preferences.d/nosnap.pref
+echo 'release a=*' >> /etc/apt/preferences.d/nosnap.pref
+echo 'Pin-Priority: -10' >> /etc/apt/preferences.d/nosnap.pref
+sudo apt update
