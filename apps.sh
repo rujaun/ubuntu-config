bb9d438ead16b13bbd583f8e5691faca44d66723
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 17:57:00 +0200

    Added default font and capitaine mouse cursor

--- apps.sh
+++ apps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install -y vim htop neofetch arandr pulseaudio pulseaudio-utils \
-					playerctl lm-sensors xbacklight
+					playerctl lm-sensors xbacklight lxappearance gnome-themes-standard
 
 sudo apt install -y nautilus --no-install-recommends --no-install-suggests
 sudo apt install -y firefox --no-install-recommends --no-install-suggests
---
+++ capitaine-cursor.sh
@@ -0,0 +1,3 @@
+sudo apt install git
+git clone https://github.com/keeferrourke/capitaine-cursors.git
+cp -pr dist/ ~/.icons/capitaine-cursors
