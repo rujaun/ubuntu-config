14b9c8f07cccff05880b2c19f6725d558917e194
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 18:16:28 +0200

    Added breeze cursor theme and configure i3 to use Xresources colors

--- apps.sh
+++ apps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt install -y vim htop neofetch arandr pulseaudio pulseaudio-utils \
-					playerctl lm-sensors xbacklight lxappearance gnome-themes-standard
+					playerctl lm-sensors xbacklight lxappearance gnome-themes-standard \
+					breeze-cursor-theme
 
 sudo apt install -y nautilus --no-install-recommends --no-install-suggests
 sudo apt install -y firefox --no-install-recommends --no-install-suggests
