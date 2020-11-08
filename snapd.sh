df204f065a885c954de5a2095c0902c5bce1a2a3
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 12:51:13 +0200

    Fixed snapd pin typo

--- snapd.sh
+++ snapd.sh
@@ -3,6 +3,6 @@
 sudo apt purge snapd
 sudo apt autoremove
 echo 'Package: snapd' > /etc/apt/preferences.d/nosnap.pref
-echo 'release a=*' >> /etc/apt/preferences.d/nosnap.pref
+echo 'Pin: release a=*' >> /etc/apt/preferences.d/nosnap.pref
 echo 'Pin-Priority: -10' >> /etc/apt/preferences.d/nosnap.pref
 sudo apt update
