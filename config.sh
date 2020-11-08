b11f2d7522f7630a6bb5d21f728fe661368d430d
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 15:18:50 +0200

    Update new i3 config

--- config.sh
+++ config.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-mkdir ~/.config/i3/
-mkdir ~/.config/i3status/
+mkdir -p ~/.config/i3/
+mkdir -p ~/.config/i3status/
 mkdir -p ~/.config/wal/templates/
-mkdir ~/.config/rofi/
-mkdir -p ~/.config/picom
+mkdir -p ~/.config/rofi/
+mkdir -p ~/.config/picom/
+mkdir -p ~/.config/displays/
 
 cp -r config/rofi/* ~/.config/rofi/
 cp -r config/i3/* ~/.config/i3/
 cp -r config/i3status/* ~/.config/i3status/
 cp -r config/wal/* ~/.config/wal/
 cp -r config/picom/* ~/.config/picom/
+cp -r config/displays/* ~/.config/displays/
