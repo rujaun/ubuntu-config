bde76f60927166f2de8e47a43f329ecd112ada3c
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 20:21:17 +0200

    Add kitty config

--- config.sh
+++ config.sh
@@ -6,6 +6,7 @@ mkdir -p ~/.config/wal/templates/
 mkdir -p ~/.config/rofi/
 mkdir -p ~/.config/picom/
 mkdir -p ~/.config/displays/
+mkdir -p ~/.config/kitty/
 
 cp -r config/rofi/* ~/.config/rofi/
 cp -r config/i3/* ~/.config/i3/
@@ -13,8 +14,8 @@ cp -r config/i3status/* ~/.config/i3status/
 cp -r config/wal/* ~/.config/wal/
 cp -r config/picom/* ~/.config/picom/
 cp -r config/displays/* ~/.config/displays/
+cp -r config/kitty/* ~/.config/kitty/
+
 cp -r config/vim/.vimrc ~/.vimrc
 cp -r config/xresources/.Xresources ~/.Xresources
-
-
 sudo cp -r config/fonts/.local.conf /etc/fonts/.local.conf
