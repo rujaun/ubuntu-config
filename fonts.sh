5ba650144b68a7d638b150bfd2746847e17588a3
Rujaun Fourie <rujaun@gmail.com>
2020-11-08 17:21:00 +0200

    Added noto fonts

--- fonts.sh
+++ fonts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y fonts-font-awesome
+sudo apt install -y fonts-font-awesome fonts-noto
 
 sudo mkdir /usr/share/fonts/WinFonts
 sudo cp ./WinFonts/* /usr/share/fonts/WinFonts/
