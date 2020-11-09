01d6ddeca83cd89a0de5b40275aa57993165ea5f
Rujaun Fourie <rujaun@gmail.com>
2020-11-09 18:09:56 +0200

    Fix core fonts bug

--- corefonts.sh
+++ corefonts.sh
@@ -3,6 +3,6 @@
 sudo apt install -y fonts-font-awesome fonts-noto
 
 sudo mkdir /usr/share/fonts/CoreFonts
-sudo cp ./WinFonts/* /usr/share/fonts/CoreFonts/
+sudo cp ./CoreFonts/* /usr/share/fonts/CoreFonts/
 sudo chmod 644 /usr/share/fonts/CoreFonts/*
 fc-cache -f -v
