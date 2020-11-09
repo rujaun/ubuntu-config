9dac6372e4e6917d3073909689b3da8adc721a16
Rujaun Fourie <rujaun@gmail.com>
2020-11-09 20:33:54 +0200

    Configured xrandr script for multi monitor setup

--- config/.bashrc
+++ config/.bashrc
@@ -12,7 +12,8 @@ alias ls='ls --color=auto'
 export PS1='\u@\h:\[\e[33m\]\w\[\e[0m\]\$ '
 
 alias wifilist='nmcli device wifi rescan && nmcli d wifi list'
-alias wificon='echo "nmcli device wifi connect <ssid> password <password>"'
+alias wificon='echo "sudo nmcli device wifi connect <ssid> password <password>"'
+alias wifidcon='echo "sudo nmcli con down <ssid>"'
 
 alias pythonv='python -m venv env'
 alias pythonva='source env/bin/activate'
--- config/displays/xrandr.sh
+++ config/displays/xrandr.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 intern=LVDS1
-extern=DP-1
+extern=DP1
 
-if xrandr | grep "$extern disconnected"; then
-    xrandr --output "$extern" --off --output "$intern" --auto
-else
-    xrandr --output "$intern" --off --output "$extern" --auto
-fi
+# For laptop config uncomment this
+#if xrandr | grep "$extern disconnected"; then
+#    xrandr --output "$extern" --off --output "$intern" --auto
+#else
+#    xrandr --output "$intern" --off --output "$extern" --auto
+#fi
