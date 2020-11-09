4b6e7bb9ce6fbe287a83c97af44b43891b32e2a9
Rujaun Fourie <rujaun@gmail.com>
2020-11-09 11:08:45 +0200

    Added bleachbit

--- apps.sh
+++ apps.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y vim htop neofetch arandr pulseaudio pulseaudio-utils \
 					playerctl lm-sensors xbacklight lxappearance gnome-themes-standard \
-					breeze qt5ct arc-theme
+					breeze qt5ct arc-theme bleachbit
 
 sudo apt install -y nautilus --no-install-recommends --no-install-suggests
 sudo apt install -y firefox --no-install-recommends --no-install-suggests
