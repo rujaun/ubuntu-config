06ae3d94f2dabcc4f16066a4cb10613655c5ffa3
Rujaun Fourie <rujaun@gmail.com>
2020-11-09 23:23:21 +0200

    picom tweaks

--- apps.sh
+++ apps.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y vim htop network-manager neofetch arandr pulseaudio pulseaudio-utils \
 					playerctl lm-sensors xbacklight lxappearance gnome-themes-standard \
-					breeze qt5ct arc-theme bleachbit
+					breeze qt5ct arc-theme bleachbit acpi
 
 sudo apt install gnome-keyring
 
