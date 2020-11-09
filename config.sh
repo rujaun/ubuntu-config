a99d93c3ba2adc9d9068c159c1682db9843c6fc0
Rujaun Fourie <rujaun@gmail.com>
2020-11-09 14:49:20 +0200

    Removed i3status

--- config.sh
+++ config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mkdir -p ~/.config/i3/
-mkdir -p ~/.config/i3status/
 mkdir -p ~/.config/wal/templates/
 mkdir -p ~/.config/rofi/
 mkdir -p ~/.config/picom/
@@ -12,7 +11,6 @@ mkdir -p ~/.config/wallpapers/
 
 cp -r config/rofi/* ~/.config/rofi/
 cp -r config/i3/* ~/.config/i3/
-cp -r config/i3status/* ~/.config/i3status/
 cp -r config/wal/* ~/.config/wal/
 cp -r config/picom/* ~/.config/picom/
 cp -r config/displays/* ~/.config/displays/
